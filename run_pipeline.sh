ffb4a7b4c663fa02bd5ea839edaebcf1b59d5762
laurentheirendt <laurent.heirendt@uni.lu>
2022-01-05 13:52:22 +0100

    integrate docker cmd

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -3,10 +3,12 @@
 logFile="logs/launch.log" # name of log file
 matlab="/usr/local/bin/matlab" # matlab executable, default: /usr/local/bin/matlab
 
-
+# Python pipeline - Estimation of Reff
 echo " + Staring Reff estimation ..." >> $logFile
-python src/reff_estimator.py
+docker run -v $(pwd)/output:/covid19/output covid19
 echo " + Reff estimation done." >> $logFile
+
+# MATLAB pipeline - Estimation of Rt
 echo " + Staring Rt estimation ..." >> $logFile
 $matlab -nodesktop -nosplash -nodisplay -nojvm -r "run('src/rt_estimator.m'); exit();" >> $logFile
 echo " + Rt estimation done." >> $logFile
