ff711d92bbd772530028626f0695c97325024318
laurentheirendt <laurent.heirendt@uni.lu>
2022-01-05 13:05:17 +0100

    default matlab exec

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-logFile="logs/launch.log"                # name of log file
-matlab="/Applications/MATLAB_R2019b.app/bin/matlab"
+
+logFile="logs/launch.log" # name of log file
+matlab="/usr/local/bin/matlab" # matlab executable, default: /usr/local/bin/matlab
+
 
 echo " + Staring Reff estimation ..." >> $logFile
 python src/reff_estimator.py
@@ -8,4 +10,4 @@ echo " + Reff estimation done." >> $logFile
 echo " + Staring Rt estimation ..." >> $logFile
 $matlab -nodesktop -nosplash -nodisplay -nojvm -r "run('src/rt_estimator.m'); exit();" >> $logFile
 echo " + Rt estimation done." >> $logFile
-
+echo " ----------------------------" >> $logFile
