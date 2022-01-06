bf4576b0eefc2be66ec88f97c83e7fde72556fa9
laurentheirendt <laurent.heirendt@uni.lu>
2022-01-06 09:07:22 +0100

    specify versions in setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ setup(
     author_email='lcsb-r3@uni.lu',
     description='Estimator for COVID-19 R(t)',
     install_requires=[
-        'pandas ',
-        'numpy ',
-        'datetime ',
-        'matplotlib ',
-        'scipy ',
-        'openpyxl'
+        'pandas>=1.3.5',
+        'numpy>=1.22.0',
+        'datetime>=4.3',
+        'matplotlib>=3.5.1',
+        'scipy>=1.7.3',
+        'openpyxl>=3.0.9'
     ],
     packages=find_packages(),
 )
