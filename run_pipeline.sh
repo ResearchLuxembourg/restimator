42a65413e7606818c7fbca3103d665c13440c2d9
Jacek Lebioda <jacek.lebioda@uni.lu>
2022-01-21 16:46:14 +0100

    fix: pipeline corrected

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-docker-compose up --no-recreate --abort-on-container-exit
+docker-compose up --no-recreate
+
+# In case you wanted to run step by step:
 # docker-compose run check --no-recreate --abort-on-container-exit
 # docker-compose run reff --no-recreate --abort-on-container-exit
 # docker-compose run rt --no-recreate --abort-on-container-exit
