611396d0bff9575fa5df9de841083d2795697d96
laurentheirendt <laurent.heirendt@uni.lu>
2022-07-19 14:20:33 +0200

    change name of docker file

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -15,7 +15,7 @@ log () {
 
 echo "found $FILE" | log find_input
 
-IMAGE=researchluxembourg/covid-reproduction-number
+IMAGE=researchluxembourg/restimator
 MOUNT="-v $PWD/input:/tool/input -v $PWD/output:/tool/output -v $PWD/logs:/tool/logs "
 DOCKER="docker run --rm $MOUNT $IMAGE julia --project "
 
